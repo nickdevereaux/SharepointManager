8878c74125644b681bb6f7e82cf61cb260739d50
SND\AndersDissing_cp <SND\AndersDissing_cp@81fbe566-5dc4-48c1-bdea-7421811ca204>
2012-07-19 10:13:47 +0000

    SharePoint Manager 2013 (beta)
    git-svn-id: https://spm.svn.codeplex.com/svn@80462 81fbe566-5dc4-48c1-bdea-7421811ca204

---
+++ SharePoint Manager 2013/SharePoint Manager 15/SharePoint Manager 2007/Forms/SplashScreen.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Keutmann.SharePointManager.Forms
+{
+    public partial class SplashScreen : Form
+    {
+        static Thread thread = null;
+        static SplashScreen frmSplash = null;
+
+        public SplashScreen()
+        {
+            InitializeComponent();
+        }
+       static public void ShowSplashScreen()
+        {
+            // Make sure it is only launched once.
+            if (frmSplash != null)
+                return;
+            thread = new Thread(new ThreadStart(SplashScreen.ShowForm));
+            thread.IsBackground = false ;
+            thread.SetApartmentState(ApartmentState.STA);
+            thread.Start();
+
+        }
+
+
+
+        static private void ShowForm()
+        {
+            frmSplash = new SplashScreen();
+            Application.Run(frmSplash);
+        }
+
+        static public SplashScreen SplashForm
+        {
+            get
+            {
+                return frmSplash;
+            }
+        }
+
+        static public void CloseForm()
+        {
+            if (frmSplash != null && !frmSplash.IsDisposed)
+            {
+                // Make it start going away.
+                frmSplash.Hide();
+                frmSplash.Dispose();
+                frmSplash = null;   
+            }
+            if (thread.IsAlive)
+            {
+                thread.Abort();
+                thread = null;  // we do not need these any more.
+            }
+        }
+
+
+
+    }
+}
